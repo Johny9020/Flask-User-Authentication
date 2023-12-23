0ce84349d549a1156cfcf1a877642ba340ab125c
Johnny Jurak <johny9020@hotmail.com>
2023-12-23 15:37:38 +0100

    Fixed imports

--- webserver/app.py
+++ webserver/app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template
 from routes.auth import auth
-from pathlib import Path
 
-THIS_FOLDER = Path(__file__).parent.resolve()
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key'
 
-
-
-app = Flask('User Authentication Python - Flask', root_path=THIS_FOLDER)
-
-app.secret_key = 'secret key'
 app.register_blueprint(auth)
-
-print(app.root_path)
 
 
 @app.route('/')
